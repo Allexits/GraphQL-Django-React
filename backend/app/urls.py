bc594bc34d6e3f508f03d145622fb1ff4950badb
Alex Kubyk <alex.kubyk@gmail.com>
2023-01-27 17:51:24 +0200

    add app/urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.conf.urls import url
-from graphene_django.views import GraphQLView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^graphql$', GraphQLView.as_view(graphiql=True)),
+    path('', include('books.urls'))
 ]
